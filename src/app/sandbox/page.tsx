225d80812c113d332484b23b79395ae5870cb3ac
Atharva-3000 <deshmukhsambhajirao9@gmail.com>
2025-02-16 01:12:07 +0530

    refactor database connection to use SingleStore; add schema for files and folders; implement sandbox page for seeding mock data

---
+++ src/app/sandbox/page.tsx
@@ -0,0 +1,32 @@
+import { db } from "~/server/db";
+import { mockFiles, mockFolders } from "~/lib/mock-data";
+import { files, folders } from "~/server/db/schema";
+
+export default function SandBoxPage() {
+
+    return <div className="flex flex-col gap-4">
+        Seed Function
+        <form action={async () => {
+            "use server";
+            console.log("Seeding Mock Data");
+            const folderInsert = await db.insert(folders).values(mockFolders.map((folder, idx) => ({
+                id: idx + 1,
+                name: folder.name,
+                parent: idx !== 0 ? 1 : null
+            }))
+            );
+            const fileInsert = await db.insert(files).values(mockFiles.map((file, idx) => ({
+                id: idx + 1,
+                name: file.name,
+                size: 50000,
+                url: file.url,
+                parent: (idx % 3) + 1,
+            }))
+            );
+            console.log("Inserted Folders", folderInsert);
+            console.log("Inserted Files", fileInsert);
+        }}>
+            <button type="submit">Seed Mock Data</button>
+        </form>
+    </div>
+}
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,8 +1,35 @@
-import { int, text, singlestoreTable } from
-"drizzle-orm/singlestore-core";
+import { int, text, index, singlestoreTableCreator, bigint } from
+  "drizzle-orm/singlestore-core";
 
-export const users = singlestoreTable("users_table", {
-  id: int("id").primaryKey().autoincrement(),
-  name: text("name"),
-  age: int("age"),
-});
+// export const users = singlestoreTable("users_table", {
+//   id: int("id").primaryKey().autoincrement(),
+//   name: text("name"),
+//   age: int("age"),
+// });
+
+export const createTable = singlestoreTableCreator((name) => `drive-tutorial_${name}`);
+
+
+export const files = createTable("files_Table", {
+  id: bigint("id", { mode: "number", unsigned: true }).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  size: int("size").notNull(),
+  url: text("url").notNull(),
+  parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
+}, (tempTable) => {
+  return [
+    index("parent_index").on(tempTable.parent)
+  ];
+},);
+
+
+
+export const folders = createTable("folders_table", {
+  id: bigint("id", { mode: "number", unsigned: true }).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  parent: bigint("parent", { mode: "number", unsigned: true }),
+}, (tempTable) => {
+  return [
+    index("parent_index").on(tempTable.parent)
+  ];
+},);
